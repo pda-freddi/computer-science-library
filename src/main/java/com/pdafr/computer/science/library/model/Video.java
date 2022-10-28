11cac4bd52bec3139b97971dba1eb9078f04ab23
Pedro Freddi <pdafreddi@gmail.com>
2022-10-28 11:50:58 -0300

    Tweak application properties and database configuration

--- src/main/java/com/pdafr/computer/science/library/model/Video.java
+++ src/main/java/com/pdafr/computer/science/library/model/Video.java
@@ -3,6 +3,7 @@ package com.pdafr.computer.science.library.model;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -14,7 +15,7 @@ public class Video {
 
     // Fields
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	
 	@Column(name="TITLE")
---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO BOOKS (TITLE, AUTHOR, CATEGORY, NUM_PAGES) VALUES ('Programming 101', 'John Smith', 2, 500);
+INSERT INTO BOOKS (TITLE, AUTHOR, CATEGORY, NUM_PAGES) VALUES ('How Computers Work', 'Andre Smith', 2, 350);
+INSERT INTO BOOKS (TITLE, AUTHOR, CATEGORY, NUM_PAGES) VALUES ('Systems Security', 'Peter Smith', 4, 600);
+INSERT INTO ARTICLES (TITLE, AUTHOR, CATEGORY, READ_TIME, LINK) VALUES ('How to setup a React project', 'Jane Smith', 3, 20, null);
+INSERT INTO ARTICLES (TITLE, AUTHOR, CATEGORY, READ_TIME, LINK) VALUES ('Common datastructures', 'Michael Smith', 4, 50, null);
+INSERT INTO ARTICLES (TITLE, AUTHOR, CATEGORY, READ_TIME, LINK) VALUES ('Configuring a firewall for Linux', 'Kevin Smith', 1, 30, null);
+INSERT INTO VIDEOS (TITLE, CHANNEL, CATEGORY, LENGTH, LINK) VALUES ('Java Spring Boot Tutorial', 'Java Tutorials', 2, 38, null);
+INSERT INTO VIDEOS (TITLE, CHANNEL, CATEGORY, LENGTH, LINK) VALUES ('State management with Redux', 'Front-end Tutorials', 3, 55, null);
+INSERT INTO VIDEOS (TITLE, CHANNEL, CATEGORY, LENGTH, LINK) VALUES ('PostgreSQL Setup', 'Database Tutorials', 4, 45, null);
